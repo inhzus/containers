c3a741fe370d2836691713029b47bec481f3ab90
inhzus <inhzus@gmail.com>
2020-02-03 20:43:51 +0800

    [Feat] SkipList erase iterator and get equal range

--- include/yaldb/skip_list.h
+++ include/yaldb/skip_list.h
@@ -14,6 +14,9 @@
 
 namespace yaldb {
 
+template<typename T, typename Comp>
+class SkipList;
+
 namespace impl {
 
 template<typename T>
@@ -21,56 +24,72 @@ struct SkipListNode {
   T value;
   SkipListNode *back;
   size_t level;
-  struct NodeLink {
-    SkipListNode *n;
-    NodeLink() : n(nullptr) {}
-  } links[1];
-
-  static void *operator new(const std::size_t size) {
-    return ::operator new(size);
-  }
-  static void operator delete(void *ptr) {
-    ::operator delete(ptr);
-  }
-  static void *operator new(const std::size_t size, const size_t level) {
-    assert(level > 0);
-    return ::operator new(size + (level - 1) * sizeof(NodeLink));
-  }
-  static void operator delete(void *ptr, const size_t) {
-    return ::operator delete(ptr);
-  }
+//  struct NodeLink {
+//    SkipListNode *n;
+//    NodeLink() : n(nullptr) {}
+//  } links[1];
+  SkipListNode **links;
+/*
+//  static void *operator new(const std::size_t size) {
+//    return ::operator new(size);
+//  }
+//  static void operator delete(void *ptr) {
+//
+//  }
+//  static void *operator new(const std::size_t size, const int level) {
+//    assert(level > 0);
+//    return ::operator new(size + (level - 1) * sizeof(NodeLink));
+//  }
+//  static void operator delete(void *ptr, const int level) {
+//    return ::operator delete(ptr, sizeof(SkipListNode)
+//        + (level - 1) * sizeof(NodeLink));
+//  }
+//  static void
+//  operator delete(void *ptr, const std::size_t size, const int level) {
+//    return ::operator delete(ptr, size);
+//  }
+ */
   SkipListNode(T value, const size_t level, SkipListNode *back) :
       value(std::move(value)), back(back), level(level) {
-    std::uninitialized_fill_n(links, level, NodeLink());
+    links = new SkipListNode *[level];
+//    std::uninitialized_fill_n(
+//        &links[0], level * sizeof(SkipListNode *), nullptr);
+    for (size_t i = 0; i < level; ++i) {
+      links[i] = nullptr;
+    }
+//    printf("construct level: %lu\n", level);
+  }
+  ~SkipListNode() {
+//    printf("destruct level: %lu\n", level);
+    delete[](links);
   }
 };
 
 template<typename T>
 class SkipListIterator {
  private:
-//  friend class SkipListNode<std::remove_const_t<T
-  template<typename> friend
-  class SkipListIterator;
-  SkipListNode<std::remove_const_t<T>> *node_;
+  template<typename U, typename Comp> friend
+  class ::yaldb::SkipList;
+  friend class SkipListNode<std::remove_const_t<T>>;
+  SkipListNode<T> *node_;
 
  public:
 //  SkipListIterator(const SkipListIterator &it) : node_(it.node_) {}
-  explicit SkipListIterator(
-      const SkipListIterator<std::remove_const_t<T>> &it) : node_(it.node_) {}
-  explicit SkipListIterator(SkipListNode<std::remove_const_t<T>> *node) :
-      node_(node) {}
-  SkipListIterator &
-  operator=(const SkipListIterator<std::remove_const_t<T>> &it) {
-    node_ = it.node_;
-    return *this;
-  }
+//  SkipListIterator(
+//      const SkipListIterator<std::remove_const_t<T>> &it) : node_(it.node_) {}
+//  template<typename U, typename std::enable_if_t<
+//      std::is_same_v<T, U> || std::is_same_v<std::remove_const_t<T>, U>,
+//      int> = 0>
+//  SkipListIterator(const SkipListIterator<U> &it) : node_(it.node_) {} // NOLINT
+  SkipListIterator(const SkipListIterator &it) : node_(it.node_) {}
+  explicit SkipListIterator(SkipListNode<T> *node) : node_(node) {}
 
-  T &operator*() { return node_->value; }
+//  T &operator*() { return node_->value; }
   const T &operator*() const { return node_->value; }
-  T *operator->() { return &node_->value; }
+//  T *operator->() { return &node_->value; }
   const T *operator->() const { return &node_->value; }
   SkipListIterator &operator++() {
-    node_ = node_->links[0].n;
+    node_ = node_->links[0];
     return *this;
   }
   SkipListIterator &operator--() {
@@ -87,12 +106,10 @@ class SkipListIterator {
     --(*this);
     return it;
   }
-  template<typename U>
-  bool operator==(const SkipListIterator<U> &it) const {
+  bool operator==(const SkipListIterator &it) const {
     return node_ == it.node_;
   }
-  template<typename U>
-  bool operator!=(const SkipListIterator<U> &it) const {
+  bool operator!=(const SkipListIterator &it) const {
     return node_ != it.node_;
   }
 //  template<typename U>
@@ -109,7 +126,7 @@ class SkipList {
  public:
   using node_type = impl::SkipListNode<T>;
   using iterator = impl::SkipListIterator<T>;
-  using const_iterator = impl::SkipListIterator<const T>;
+  using const_iterator = iterator;
 
  private:
   [[nodiscard]] size_t RandomLevel() const;
@@ -127,12 +144,15 @@ class SkipList {
 
  public:
   explicit SkipList(Comp comp = std::less<T>());  // NOLINT
+  ~SkipList();
 
-  size_t size() const { return length_; }
-  iterator begin() { return iterator(head_->links[0].n); }
+  size_t Size() const { return length_; }
+  bool Empty() const { return length_ == 0; }
+
+  iterator begin() { return iterator(head_->links[0]); }
   iterator end() { return iterator(tail_); }
   [[nodiscard]] const_iterator begin() const {
-    return const_iterator(head_->links[0].n);
+    return const_iterator(head_->links[0]);
   }
   [[nodiscard]] const_iterator end() const {
     return const_iterator(tail_);
@@ -140,10 +160,9 @@ class SkipList {
 
   iterator Insert(T value);
   iterator Erase(const T &value);
-  iterator Find(const T &value);
-  const_iterator Find(const T &value) const {
-    return const_iterator(const_cast<SkipList *>(this)->Find(value));
-  }
+  iterator Erase(iterator it);
+  iterator Find(const T &value) const;
+  std::pair<iterator, iterator> EqualRange(const T &value) const;
 };
 
 template<typename T, typename Comp>
@@ -160,8 +179,8 @@ typename SkipList<T, Comp>::node_type *
 SkipList<T, Comp>::FindPrev(const T &value) const {
   node_type *cur = head_;
   for (size_t i = kMaxLevel - 1; i != size_t() - 1; --i) {
-    while (cur->links[i].n != tail_ && comp_(cur->links[i].n->value, value)) {
-      cur = cur->links[i].n;
+    while (cur->links[i] != tail_ && comp_(cur->links[i]->value, value)) {
+      cur = cur->links[i];
     }
   }
   return cur;
@@ -171,8 +190,8 @@ typename SkipList<T, Comp>::node_type *
 SkipList<T, Comp>::FindPrev(const T &value, node_type **prev) const {
   node_type *cur = head_;
   for (size_t i = kMaxLevel - 1; i != size_t() - 1; --i) {
-    while (cur->links[i].n != tail_ && comp_(cur->links[i].n->value, value)) {
-      cur = cur->links[i].n;
+    while (cur->links[i] != tail_ && comp_(cur->links[i]->value, value)) {
+      cur = cur->links[i];
     }
     prev[i] = cur;
   }
@@ -187,29 +206,39 @@ SkipList<T, Comp>::SkipList(Comp comp) :
       bool, std::invoke_result_t<Comp, const T &, const T &>>);
   std::random_device rd;
   rand_gen_ = std::mt19937(rd());
-  head_ = new(kMaxLevel)node_type(T(), kMaxLevel, nullptr);
-  tail_ = new(kMaxLevel)node_type(T(), kMaxLevel, head_);
+  head_ = new node_type(T(), kMaxLevel, nullptr);
+  tail_ = new node_type(T(), kMaxLevel, head_);
   for (size_t i = 0; i < kMaxLevel; ++i) {
-    head_->links[i].n = tail_;
+    head_->links[i] = tail_;
   }
+}
+template<typename T, typename Comp>
+SkipList<T, Comp>::~SkipList() {
+  while (tail_ != nullptr) {
+    node_type *node = tail_->back;
+    delete (tail_);
+    tail_ = node;
+  }
+//  ::operator delete(head_, head_->level);
+//  ::operator delete(tail_, tail_->level);
 }
 template<typename T, typename Comp>
 typename SkipList<T, Comp>::iterator
 SkipList<T, Comp>::Insert(T value) {
   auto prev = std::make_unique<node_type *[]>(kMaxLevel);;
-  node_type *cur = FindPrev(value, prev.get()), *next = cur->links[0].n;
+  node_type *cur = FindPrev(value, prev.get());
 //  if (next != tail_ &&
 //      !comp_(next->value, value) &&
 //      !comp_(value, next->value)) {
 //    return end();
 //  }
   size_t level = RandomLevel();
-  auto *node = new(level)node_type(std::move(value), level, cur);
+  auto *node = new node_type(std::move(value), level, cur);
   for (size_t i = 0; i < level; ++i) {
-    node->links[i].n = prev[i]->links[i].n;
-    prev[i]->links[i].n = node;
+    node->links[i] = prev[i]->links[i];
+    prev[i]->links[i] = node;
   }
-  next->back = cur;
+  node->links[0]->back = node;
   ++length_;
   return iterator(node);
 }
@@ -220,24 +249,26 @@ SkipList<T, Comp>::Erase(const T &value) {
   FindPrev(value, prev.get());
   node_type *first = tail_, *last = first;
   for (size_t i = kMaxLevel - 1; i != size_t() - 1; --i) {
-    for (node_type *node = prev[i]->links[i].n;
-         prev[i]->links[i].n != tail_ &&
-             !comp_(prev[i]->links[i].n->value, value) &&
-             !comp_(value, prev[i]->links[i].n->value);
-         node = prev[i]->links[i].n) {
-      prev[i]->links[i].n = node->links[i].n;
+    for (node_type *node = prev[i]->links[i];
+         node != tail_ &&
+             !comp_(node->value, value) &&
+             !comp_(value, node->value);
+         node = prev[i]->links[i]) {
+      prev[i]->links[i] = node->links[i];
       if (i == 0) {
         if (first == tail_) {
           first = node;
         }
-        last = node->links[i].n;
+        last = node->links[i];
       }
     }
   }
   if (first != last) {
+    last->back = first->back;
     while (first != last) {
       node_type *tmp = first;
-      first = first->links[0].n;
+      first = first->links[0];
+//      ::operator delete(tmp, tmp->level);
       delete tmp;
       --length_;
     }
@@ -248,8 +279,38 @@ SkipList<T, Comp>::Erase(const T &value) {
 }
 template<typename T, typename Comp>
 typename SkipList<T, Comp>::iterator
-SkipList<T, Comp>::Find(const T &value) {
-  node_type *cur = FindPrev(value), *next = cur->links[0].n;
+SkipList<T, Comp>::Erase(iterator it) {
+  auto prev = std::make_unique<node_type *[]>(kMaxLevel);
+  FindPrev(*it, prev.get());
+  bool is_contained = false;
+  for (size_t i = kMaxLevel - 1; i != size_t() - 1; --i) {
+    for (node_type *cur = prev[i], *next = cur->links[i];
+         next != tail_ &&
+             !comp_(next->value, *it) &&
+             !comp_(*it, next->value);
+         cur = next, next = cur->links[i]) {
+      if (next == it.node_) {
+        is_contained = true;
+        cur->links[i] = next->links[i];
+        break;
+      }
+    }
+  }
+  if (is_contained) {
+    node_type *back = it.node_->back;
+    it.node_->links[0]->back = back;
+    delete it.node_;
+//    ::operator delete(it.node_, it.node_->level);
+    --length_;
+    return iterator(back);
+  } else {
+    return end();
+  }
+}
+template<typename T, typename Comp>
+typename SkipList<T, Comp>::iterator
+SkipList<T, Comp>::Find(const T &value) const {
+  node_type *cur = FindPrev(value), *next = cur->links[0];
   if (next != tail_ &&
       !comp_(next->value, value) &&
       !comp_(value, next->value)) {
@@ -258,14 +319,32 @@ SkipList<T, Comp>::Find(const T &value) {
     return end();
   }
 }
-
-void StaticTest() {
-  auto f = [](int, int) {
-    return true;
-  };
-  SkipList<int, decltype(f)> s(f);
+template<typename T, typename Comp>
+std::pair<typename SkipList<T, Comp>::iterator,
+          typename SkipList<T, Comp>::iterator>
+SkipList<T, Comp>::EqualRange(const T &value) const {
+  node_type *cur = FindPrev(value), *first = cur->links[0], *last = first;
+  while (last != tail_ &&
+      !comp_(last->value, value) &&
+      !comp_(value, last->value)) {
+    last = last->links[0];
+  }
+  return std::make_pair(iterator(first), iterator(last));
 }
 
 }  // namespace yaldb
+
+namespace std {
+
+template<typename T>
+struct iterator_traits<yaldb::impl::SkipListIterator<T>> {
+  typedef bidirectional_iterator_tag iterator_category;
+  typedef T value_type;
+  typedef ptrdiff_t difference_type;
+  typedef T *pointer;
+  typedef T &reference;
+};
+
+}  // namespace std
 
 #endif  // YALDB_SKIP_LIST_H_
